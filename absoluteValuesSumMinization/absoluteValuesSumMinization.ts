25ed7386a3a16a67f5147ffe8c05e6e0d96e52ab
Błażej Tempski <blazej33.9@gmail.com>
2019-04-10 20:08:59 +0200

    First algorithm solve

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,12 @@
-function absoluteValuesSumMinimization(a: number[]): number {
-
+function absoluteValuesSumMinimization(a :number[]) : number {
+    if (a.length % 2 === 0){
+        return a[a.length / 2 - 1]
+    }
+    else{
+        return a[(a.length+1)/2 - 1]
+    }
 }
-
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+console.log(absoluteValuesSumMinimization([2, 4, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
