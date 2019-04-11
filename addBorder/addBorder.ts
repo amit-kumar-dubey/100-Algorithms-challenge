3a05e54c5042f72d6b7e41dd804dc2ecb9f4a10a
Błażej Tempski <blazej33.9@gmail.com>
2019-04-11 17:28:57 +0200

    Third algorithm solve

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,20 @@
 function addBorder(picture: string[]): string[] {
 
+    var length = picture[0].length;
+    var border = "**";
+
+    for (let i = 0; i < length; i++) {
+        border += "*";
+    }
+
+    for (let i = 0; i < picture.length; i++) {
+        picture[i] = "*" + picture[i] + "*";
+    }
+
+    picture.unshift(border);
+    picture.push(border);
+
+    return picture;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
