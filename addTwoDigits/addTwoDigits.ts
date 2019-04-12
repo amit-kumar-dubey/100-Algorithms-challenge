37a06b24949d51c7fd9d9546404c7a25ea8212c5
Błażej Tempski <blazej33.9@gmail.com>
2019-04-12 21:52:21 +0200

    Fourth alghoritm solve

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,12 @@
-function addTwoDigits(n: any): number {
+function addTwoDigits(n: any) {
+    var number : string = n.toString();
+    var numbers : any[] = number.split('');
+    
+    for (let i=0; i < 2; i++){
+        numbers[i] = parseInt(numbers[i])
+    }
 
+    return numbers[0] + numbers[1] ;
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29));
